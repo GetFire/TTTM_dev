0418d64e60a862dd37cf616b4fe5ca7e5f5c1f8e
Volodymyr Maystrovy <volodymyr.maystrovy@pearlconsulting.no>
2017-11-03 00:05:56 +0200

    Translate 4

--- sources/tournoi/exception/TournoiException.java
+++ sources/tournoi/exception/TournoiException.java
@@ -10,7 +10,7 @@ public class TournoiException extends Exception
      * 
      */
     private static final long serialVersionUID = 1L;
-    public static TournoiException MAX_SIZE_EXCEPTION = new TournoiException("Trop de joueurs pour ce tournois");
+    public static TournoiException MAX_SIZE_EXCEPTION = new TournoiException("Слишком много игроков для этого турнира");
 	
 	public TournoiException(String message)
 	{
